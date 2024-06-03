e836cf342d55f66baf28baabf232d62629d6137d
Usuario <Usuario@CPD03-M07>
2024-06-03 13:45:05 -0300

    nao finalizado

--- MauiAppecinemas/Categorias/Filmesco/Debieloide.xaml.cs
+++ MauiAppecinemas/Categorias/Filmesco/Debieloide.xaml.cs
@@ -6,4 +6,9 @@ public partial class Debieloide : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void voltar_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Comedia());
+    }
 }
